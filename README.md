0572f0338514546dbeeb85a3471bbd322b8dccce
sohaibHassan <48724541+sohaib115@users.noreply.github.com>
2024-02-10 21:57:15 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Login-App-using-Django
+ Login  web page with authentication using HTML & Django
---
+++ home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,logout,login
+# passw Sobi@12345$$$
+
+
+
+# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request,"index.html")
+
+
+def loginUser(request):
+      if request.method=="POST":
+          username=request.POST.get('username')
+          password=request.POST.get('password')
+          print(request.user)
+          print(username,password)
+          user = authenticate(username=username, password=password)
+          if user is not None:
+              
+              login(request,user)
+              return redirect("/")
+          else:
+             return render(request,"login.html") 
+      return render(request,"login.html")          
+ 
+    
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
